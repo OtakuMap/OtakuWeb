96186de63b459eb45b9c002dcd6b7e5806430a57
sun <jioseon@gmail.com>
2025-01-13 15:41:12 +0900

    커버, 마이페이지, 좋아요, 메인, 카테고리 1차

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
 import RouteManagement from './components/RouteManagement';
+import SavedPlaces from './components/SavedPlaces';
+import SavedEvents from './components/SavedEvents';
 import Navbar from './components/common/Navbar';
+import MyPage from './components/MyPage';
+import Cover from './components/Cover';
+import Category from './components/category';
 
 const AppContainer = styled.div`
   position: relative;
@@ -11,10 +17,24 @@ const AppContainer = styled.div`
 
 const App: React.FC = () => {
   return (
-    <AppContainer>
-      <Navbar />
-      <RouteManagement />
-    </AppContainer>
+    <Router>
+      <AppContainer>
+        <Navbar />
+        <Routes>
+          {/* 기본 루트: RouteManagement */}
+          <Route path="/" element={<RouteManagement />} />
+          {/* 찜 페이지 */}
+          <Route path="/saved-places" element={<SavedPlaces />} />
+          {/* 저장한 이벤트 페이지 */}
+          <Route path="/saved-events" element={<SavedEvents />} />
+          {/* 마이페이지 */}
+          <Route path="/my-page" element={<MyPage />} />
+          {/* 추가된 경로 */}
+          <Route path="/cover" element={<Cover />} />
+          <Route path="/category" element={<Category />} />
+        </Routes>
+      </AppContainer>
+    </Router>
   );
 };
 
