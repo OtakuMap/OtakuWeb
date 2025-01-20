773422a63c75316949a1ea134912390cd0a1ba3a
xlzbthxyjkoo <happyyj01@naver.com>
2025-01-20 23:39:15 +0900

    feat: 이벤트, 명소 필터 버튼 구현

--- src/components/map/FilterButton.tsx
+++ src/components/map/FilterButton.tsx
@@ -2,22 +2,23 @@ import React, { useState } from 'react';
 import * as S from '../../styles/map/FilterButton.styles';
 
 interface FilterButtonProps {
-  onFilterChange?: (isActive: boolean) => void;
+  onFilterChange?: (filter: 'spot' | 'event') => void;
 }
 
 const FilterButton: React.FC<FilterButtonProps> = ({ onFilterChange }) => {
-  const [isActive, setIsActive] = useState(false);
+  const [isEventMode, setIsEventMode] = useState(false);
 
-  const handleToggle = () => {
-    const newState = !isActive;
-    setIsActive(newState);
-    onFilterChange?.(newState);
+  const handleClick = () => {
+    setIsEventMode(!isEventMode);
+    onFilterChange?.(isEventMode ? 'spot' : 'event');
   };
 
   return (
-    <S.FilterContainer>
-      <S.ToggleButton $isActive={isActive} onClick={handleToggle} />
-      <S.FilterText>명소만</S.FilterText>
+    <S.FilterContainer onClick={handleClick}>
+      <S.FilterOption $isActive={!isEventMode} />
+      <S.FilterOption $isActive={isEventMode} />
+      <S.LeftText $isVisible={isEventMode}>이벤트만</S.LeftText>
+      <S.RightText $isVisible={!isEventMode}>명소만</S.RightText>
     </S.FilterContainer>
   );
 };
