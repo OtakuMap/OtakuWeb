2289051f593cf4cee80ee9585c012cd47586cfef
xlzbthxyjkoo <happyyj01@naver.com>
2025-02-21 11:38:29 +0900

    feat: type

--- src/api/review/review.ts
+++ src/api/review/review.ts
@@ -1,7 +1,12 @@
-// api/review/review.ts
 import instance from '@/api/axios';
 import { ApiResponse, ReviewDetail, ReviewType } from '@/types/review/review';
 
+/**
+ * 리뷰 상세 정보를 가져오는 함수
+ * @param reviewId 리뷰 ID
+ * @param type 리뷰 타입 (기본값: 'PLACE')
+ * @returns API 응답
+ */
 export const getReviewDetail = (reviewId: number, type: ReviewType = 'PLACE') => {
   return instance
     .get<ApiResponse<ReviewDetail>>(`/reviews/${reviewId}`, {
